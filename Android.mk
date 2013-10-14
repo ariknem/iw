f5634a86da00ac127cfb114f042d42c6858a6344
Arik Nemtsov <arik@wizery.com>
2013-10-14 15:08:37 +0200

    android: make the compilation work
    Adds a bit of ugliness..
    
    Signed-off-by: Arik Nemtsov <arik@wizery.com>

--- Android.mk
+++ Android.mk
@@ -4,9 +4,17 @@ IW_SOURCE_DIR := $(LOCAL_PATH)
 include $(CLEAR_VARS)
 
 NO_PKG_CONFIG=y
-include $(LOCAL_PATH)/Makefile
+#include $(LOCAL_PATH)/Makefile
 
-LOCAL_SRC_FILES := $(patsubst %.o,%.c,$(OBJS)) android-nl.c
+OBJS := iw.c genl.c event.c info.c phy.c \
+	interface.c ibss.c station.c survey.c util.c \
+	mesh.c mpath.c scan.c reg.c version.c \
+	reason.c status.c connect.c link.c offch.c ps.c cqm.c \
+	bitrate.c wowlan.c coalesce.c roc.c p2p.c
+OBJS += sections.c
+OBJS += hwsim.c
+
+LOCAL_SRC_FILES := $(OBJS) android-nl.c
 
 LOCAL_C_INCLUDES := \
 	$(LOCAL_PATH) \
@@ -16,7 +24,7 @@ LOCAL_CFLAGS += -DCONFIG_LIBNL20
 LOCAL_LDFLAGS := -Wl,--no-gc-sections
 #LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_TAGS := eng
-LOCAL_SHARED_LIBRARIES := libnl_2
+LOCAL_STATIC_LIBRARIES := libnl_2
 LOCAL_MODULE := iw
 
 $(IW_SOURCE_DIR)/version.c:
